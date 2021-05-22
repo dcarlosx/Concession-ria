2398d41b3316e2eeaeb41798d2424af05c6a8cc0
Diego Carlos <79551200+dcarlosx@users.noreply.github.com>
2021-05-21 21:52:33 -0300

    Update views.py

--- views.py
+++ views.py
@@ -1,12 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from app.forms import ClientesForm
+from app.models import Clientes
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    data = { }
+    data['db'] = Clientes.objects.all()
+    return render(request, 'index.html', data)
 
 
 def form (request):
     data = { }
     data['form'] = ClientesForm()
     return render(request, 'form.html' , data)
+
+
+def create(request):
+    form = ClientesForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+
+def view(request, pk):
+    data = { }
+    data['db'] = Clientes.objects.get(pk=pk)
+    return render(request, 'view.html', data)
